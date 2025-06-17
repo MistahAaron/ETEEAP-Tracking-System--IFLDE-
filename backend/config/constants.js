ab78178d094c7a3e6a339e0435acc30e350f0016
MistahAaron <aaron26gatongay@gmail.com>
2025-06-17 14:49:08 +0800

    railway attempts 5

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -4,5 +4,5 @@ module.exports = {
   JWT_SECRET:
     process.env.JWT_SECRET ||
     "21b4f89a96ecde2d4d88285d15da09017466707bfd345b7d02bd587fb6981e6a",
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 8080,
 };
